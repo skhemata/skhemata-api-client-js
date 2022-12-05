0f80675593109a73e9c7871db40a512a4fa6951a
alexey432 <alexorl432@gmail.com>
2022-12-05 14:57:08 -0800

    spelling error fix

--- src/Subscription.ts
+++ src/Subscription.ts
@@ -1,27 +1,27 @@
-import { APICall, APIRequest, RequestMethod } from './APICall';
+import { APICall, APIRequest, RequestMethod } from "./APICall";
 
-export interface Subscription{
+export interface Subscription {
   data: any;
   api: APICall;
   id: number;
 }
 
-interface SubscriptionData{
-  token: string,
-  email: string,
-  first_name: string
-  last_name: string,
-  street1: string,
-  mail_code: any,
-  city_id: number,
-  subscription_plan_id: number,
-  portal_name: string,
-  terms: number,
-  use_sca: number,
-  source?: string,
-  create_sample_data: number,
-  secure_site: number,
-  custom_domain: string,
+interface SubscriptionData {
+  token: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  street1: string;
+  mail_code: any;
+  city_id: number;
+  subscription_plan_id: number;
+  portal_name: string;
+  terms: number;
+  use_sca: number;
+  source?: string;
+  create_sample_data: number;
+  secure_site: number;
+  custom_domain: string;
 }
 
 export class Subscription implements Subscription {
@@ -51,13 +51,13 @@ export class Subscription implements Subscription {
       mail_code: this.data.mail_code,
       city_id: this.data.city_id,
       subscription_plan_id: this.data.subscription_plan_id,
-      portal_name: this.data.portal_name.trim().replace(/ /g, '_'),
+      portal_name: this.data.portal_name.trim().replace(/ /g, "_"),
       terms: this.data.terms,
       use_sca: 1,
       source: this.data.source,
       create_sample_data: this.data.create_sample_data,
       secure_site: 0,
-      custom_domain: 'custom',
+      custom_domain: "custom",
     };
     /*
     if (this.data.plan.addon) {
@@ -66,28 +66,36 @@ export class Subscription implements Subscription {
 
     const request: APIRequest = {
       requestMethod: RequestMethod.POST,
-      endpoint: 'subscription',
+      endpoint: "subscription",
       data: subscriptionData,
     };
 
     return new Promise((resolve, reject) => {
-      this.api.send(request)
+      this.api
+        .send(request)
         .then((success) => {
-          if (success.sca_status === 'requies_action' || success.sca_status === 'requires_source_action') {
-            stripe.confirmCardPayment(success.client_secret).then((pi: any) => {
-              if (pi.error) {
-                reject(pi.error);
-              } else {
-                resolve(success);
-              }
-            }).catch((error: any) => {
-              reject(error);
-            });
+          if (
+            success.sca_status === "requires_action" ||
+            success.sca_status === "requires_source_action"
+          ) {
+            stripe
+              .confirmCardPayment(success.client_secret)
+              .then((pi: any) => {
+                if (pi.error) {
+                  reject(pi.error);
+                } else {
+                  resolve(success);
+                }
+              })
+              .catch((error: any) => {
+                reject(error);
+              });
           } else {
-            console.log('no auth, resolving');
+            console.log("no auth, resolving");
             resolve(success);
           }
-        }).catch((error) => {
+        })
+        .catch((error) => {
           reject(error);
         });
     });
